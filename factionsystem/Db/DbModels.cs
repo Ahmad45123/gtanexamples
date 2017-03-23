aad4743260ab7667ff5b1869e3a98079c644df97
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-03-23 14:14:22 +0200

    Added user table and commands for each fac/div.

--- factionsystem/Db/DbModels.cs
+++ factionsystem/Db/DbModels.cs
@@ -15,6 +15,9 @@ namespace factionsystem.Db
         public int FactionId { get; set; }
         public string FactionName { get; set; }
 
+        //Since EF doesn't support lists, we will have to do some manual work.
+        public string Commands { get; set; }
+
         //This is to mantain that divisions are related to this.
         public virtual ICollection<Division> Divisions { get; set; }
     }
@@ -29,9 +32,30 @@ namespace factionsystem.Db
         [ForeignKey("Faction")]
         public int FactionId { get; set; }
 
+        //Since EF doesn't support lists, we will have to do some manual work.
+        public string Commands { get; set; }
+
         // This will keep track of the faction this division belongs too
         public virtual Faction Faction { get; set; }
     }
 
+    
+    //Ofcourse this is not an ideal user system, there is even no passwords.. but just for showcase :D
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string Username { get; set; }
 
+        [ForeignKey("Faction")]
+        public int FactionId { get; set; }
+
+        [ForeignKey("Division")]
+        public int DivisionId { get; set; }
+
+        // This will keep track of the faction and div this user belongs too
+        public virtual Faction Faction { get; set; }
+        public virtual Division Division { get; set; }
+    }
 }
