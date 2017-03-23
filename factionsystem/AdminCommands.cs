ec4131cab40715ade48bac70c9625edc6137ca76
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-03-23 13:58:37 +0200

    Added /createfaction.

---
+++ factionsystem/AdminCommands.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using factionsystem.Db;
+using GTANetworkServer;
+
+namespace factionsystem
+{
+    class AdminCommands
+    {
+        [Command("createfaction", ACLRequired = true, GreedyArg = true)]
+        public void CreateFaction(Client player, string factionName)
+        {
+            var dbContext = new FactionsDbContext();
+            if (dbContext.Factions.Count(x => x.FactionName == factionName) == 0)
+            {
+                //Create faction.
+                Faction fac = new Faction() {FactionName = factionName};
+                dbContext.Factions.Add(fac);
+                dbContext.SaveChanges();
+                API.shared.sendChatMessageToPlayer(player, $"Faction with name \"{factionName}\" was sucessfully created.");
+            }
+            else
+             API.shared.sendChatMessageToPlayer(player, "Faction with that name already exists.");
+        }
+    }
+}
