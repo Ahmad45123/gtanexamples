d1eaa797acaee86e4e7cd788fc0351f31c69f084
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-03-28 16:06:43 +0200

    Added to check for command perms.

--- factionsystem/CheckForCommandPerm.cs
+++ factionsystem/CheckForCommandPerm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using factionsystem.Db;
@@ -20,6 +21,7 @@ namespace factionsystem
         {
             //First get all the faction methods.
             var factionMethods = Assembly.GetExecutingAssembly().GetTypes()
+                      .Where(x => x.BaseType == typeof(Script))
                       .SelectMany(t => t.GetMethods())
                       .Where(m => m.GetCustomAttributes(typeof(FactionCommandAttribute), false).Length > 0)
                       .ToArray();
@@ -28,10 +30,37 @@ namespace factionsystem
             foreach (var fac in factionMethods)
             {
                 CommandAttribute cmd = (CommandAttribute)fac.GetCustomAttributes(typeof(CommandAttribute), false).First();
-                if (cmd.CommandString == command)
+                if (cmd.CommandString == command.Remove(0, 1))
                 {
-                    //TODO: code to check if user has permissions to this command by getting their faction/div and then comparing.
-                    return;
+                    //We will cancel by default and remove the cancel later if has perms.
+                    cancel.Cancel = true;
+                    cancel.Reason = "You don't have enough permissions to access this command.";
+
+                    using (FactionsDbContext dbContext = new FactionsDbContext())
+                    {
+                        //First of all, get the user object that corrsponds to this user or create new.
+                        var userName = API.getPlayerName(sender);
+                        var user = dbContext.Users.SingleOrDefault(x => x.Username == userName) ??
+                                   dbContext.Users.Add(new User() {Username = userName});
+                        //Check div perms for this command.
+                        if(user.Division != null)
+                        {
+                            var cmds = user.Division.Commands.Split(',');
+                            foreach (var dCmd in cmds)
+                            {
+                                if (dCmd == cmd.CommandString)
+                                {
+                                    cancel.Cancel = false;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if(cancel.Cancel)
+                            API.sendChatMessageToPlayer(sender, "You don't have enough permissions to access this command.");
+
+                        dbContext.SaveChanges();
+                    }
                 }
             }
         }
